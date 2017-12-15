21047b7a2c8e97276fbf143b5970b3dae94b0b4d
ashco <ashchristie@gmail.com>
2017-12-15 13:32:57 -0800

    new move fnc logic

--- js/source.js
+++ js/source.js
@@ -1,6 +1,9 @@
 //Selected card number
-var selected = false;
+var startNum = false;
 var startArr;
+var targetNum;
+var targetArr;
+
 //var targetArr;
 //array of 52 cards
 var cards = [];
@@ -85,6 +88,7 @@ function cycle(){
   //update images of deck and drawn
   deckImg();
   drawnImg();
+  addClick();
 }
 
 
@@ -101,7 +105,10 @@ function drawnImg(){
   if(board.drawn.length === 0){
     $('.drawn').find('img').attr('src', './img/extra/card_empty.png');
   }else{
-    $('.drawn').find('img').attr('src', './img/cards/card_' + cards[board.drawn[board.drawn.length-1]].suit + '_' + cards[board.drawn[board.drawn.length-1]].name + '.png');
+    $('.drawn').find('img').removeAttr();
+    $('.drawn').find('img')
+    .attr('src', './img/cards/card_' + cards[board.drawn[board.drawn.length-1]].suit + '_' + cards[board.drawn[board.drawn.length-1]].name + '.png')
+    .attr('data-cardnum', board.drawn[board.drawn.length-1]);
   }
 }
 
@@ -115,7 +122,7 @@ function topImg(){
       $('.' + stack).find('img').last()
       .attr('class', 'flipped')
       .attr('src', './img/cards/card_' + cards[(board[stack][board[stack].length-1])].suit + '_' + cards[(board[stack][board[stack].length-1])].name + '.png')
-      .attr('data-cardNum', board[stack][board[stack].length-1]);
+      .attr('data-cardnum', board[stack][board[stack].length-1]);
     }
   }  
 }
@@ -139,122 +146,189 @@ function removeCardImg(){
   $('.' + startArr + ' img:nth-last-child(2)').remove();
 }
 
-
-//determines if card is selected or not
-function decider(){
-  var focus = $(this).parent().attr('class');
-  console.log('decider fnc: this is ', this, 'focus is', focus);
-  //SELECTOR
-  if(selected === false){
-    //cannot select empty array
-    if(board[focus].length === 0){
+function move(){
+  //set start info
+  if(startNum === false){
+    console.log('START TURN');
+    startNum = $(this).data('cardnum');
+    startArr = $(this).parent().attr('class');
+    //No cards in selected array
+    if(board[startArr].length === 0){
       console.log('no cards in column!');
-    //this will update selected var with card number
+      return;
     }else{
-      selected = board[focus][board[focus].length-1];
-      console.log('Selected: ', selected);
-      //adds highlight img
       var selectImg = $('<img>');
       selectImg.addClass('selected').attr('src', './img/extra/card_selected.png');
       $(this).parent().append(selectImg);
     }
-    return;
-  }
-  //EXECUTOR
-  var target = board[focus][board[focus].length-1] || -1;
-  if(focus === 'hearts' ||
-  focus === 'diamonds' ||
-  focus === 'spades' ||
-  focus === 'clubs'){
-    moveSuit(selected, target, focus);
+  //set target info
   }else{
-    moveCard(selected, target, focus);
+    console.log('TARGET TURN');
+    targetNum = $(this).data('cardnum') || -1;
+    targetArr = $(this).parent().attr('class');
+    if(targetArr === 'hearts' || targetArr === 'diamonds' || targetArr === 'spades' || targetArr === 'clubs'){
+      moveSuit(startNum, startArr, targetNum, targetArr);
+    }else{
+      moveCard(startNum, startArr, targetNum, targetArr);
+    }
+    console.log('This is:', $(this));
+    console.log('StartNum param  is ', startNum);
+    console.log('StartArr Col is ', startArr);  
+    console.log('TargetNum param  is ', targetNum);
+    console.log('TargetArr Col is ', targetArr);
+    //run to reset
+    deselect();
   }
-  deselect();
-  topImg();
-  addClick();
 }
 
+
+function moveSuit(){
+  console.log('moveSuit');
+}
+
+function moveCard(){
+  console.log('moveCard');
+}
+
+
+// //determines if card is selected or not
+// function decider(){
+//   var focus = $(this).parent().attr('class');
+//   selectedCardNum = $(this).data('cardnum');
+//   console.log('decider fnc: this is ', this, 'focus is', focus, 'selectedCardNum is', selectedCardNum);
+//   //SELECTOR
+//   if(selected === false){
+//     //cannot select empty array
+//     if(board[focus].length === 0){
+//       console.log('no cards in column!');
+//     //this will update selected var with card number
+//     }else{
+//       selected = board[focus][board[focus].length-1];
+//       console.log('Selected: ', selected);
+//       //adds highlight img
+//       var selectImg = $('<img>');
+//       selectImg.addClass('selected').attr('src', './img/extra/card_selected.png');
+//       $(this).parent().append(selectImg);
+//     }
+//     return;
+//   }
+//   //EXECUTOR
+//   var target = board[focus][board[focus].length-1] || -1;
+//   if(focus === 'hearts' ||
+//   focus === 'diamonds' ||
+//   focus === 'spades' ||
+//   focus === 'clubs'){
+//     moveSuit(selected, target, focus);
+//   }else{
+//     moveCard(selected, target, focus);
+//   }
+//   deselect();
+//   topImg();
+//   addClick();
+// }
+
+
 function deselect(){
-  selected = false;
+  startNum = false;
+  startArr = false;
+  targetNum = false; 
+  targetArr = false;
   $('.selected').remove();
 }
 
 
-function moveCard(start, target, targetArr){
-  //finds names of arrays that contain start/target values
-  for(var area in board){
-    if(board[area].indexOf(start) >= 0){
-      startArr = area;
-    }
-  }
-  // console.log('this is the TARGET length', board[targetArr].length);
-  // console.log('StartArr Col is ', startArr);  
-  // console.log('TargetArr Col is ', targetArr);
-  // console.log('Start param  is ', start);
-  // console.log('Target param  is ', target);
 
-  //switch statement?
-  if(cards[start].value !== 13 &&
-  board[targetArr].length === 0){
-    console.log('NO MOVE: card is not a king!');
-    return;
-  //Move king into empty array
-  }else if(cards[start].value === 13 && 
-  board[targetArr].length === 0){
-    //find targets array
-    board[targetArr].push(board[startArr].pop());
-    addCardImg(targetArr);
-    removeCardImg();
-    console.log('MOVE: king card added to empty column!');
-    return;
-  //move card on top of card with alt color
-  }else if(cards[start].color !== cards[target].color &&
-  //move card on top of card with +1 higher value 
-  (cards[start]).value + 1 === (cards[target]).value){
-    //find targets array
-    board[targetArr].push(board[startArr].pop());
-    addCardImg(targetArr);
-    removeCardImg();
-    console.log('card moved!');
-    return;
-  }else{
-    console.log('card cannot move');
-    return;
-  }
-}
+// function moveCard2(startCardNum){
+//   //identifies column that card is in
+//   for(var area in board){
+//     if(board[area].indexOf(startCardNum) >= 0){
+//       startArr = area; 
+//       // console.log(startArr);
+//     }
+//   }
+//   if(cards[startCardNum].value !== 13 &&
+//   board[targetArr].length === 0){
+//     console.log('No move, card is not king');
+//     return;
+//   }
+// }
 
-//logic that determines if card can be moved to sideline storage area
-function moveSuit(start, target, targetArr){
-  //REPEAT - can turn into function
-  for(var area in board){
-    if(board[area].indexOf(start) >= 0){
-      startArr = area;
-    }
-  }
-  // console.log('start is ', start, 'target is : ', target, 'startArr is: ', startArr, 'targetArr is :', targetArr);
-  //start card is not same suit
-  if(cards[start].suit !== targetArr){
-    console.log('suit does not match!');
-    return;
-  }else if(cards[start].value !== 1 && board[targetArr].length === 0){
-    console.log('not an Ace, idiot!');
-    return;
-  }else if(cards[start].value === 1 && board[targetArr].length === 0){
-    board[targetArr].push(board[startArr].pop());
-    addCardImg(targetArr);
-    removeCardImg();
-    console.log('added Ace');
-    return;
-  }else if((cards[start]).value === (cards[target].value+1)){
-    board[targetArr].push(board[startArr].pop());
-    removeCardImg();
-    console.log('stored suit');
-    return;
-  }else{
-    console.log('card is not 1+ in value');
-  }
-}
+
+
+// function moveCard(start, target, targetArr){
+//   //finds names of arrays that contain start/target values
+//   for(var area in board){
+//     if(board[area].indexOf(start) >= 0){
+//       startArr = area;
+//     }
+//   }
+//   // console.log('this is the TARGET length', board[targetArr].length);
+//   // console.log('StartArr Col is ', startArr);  
+//   // console.log('TargetArr Col is ', targetArr);
+//   // console.log('Start param  is ', start);
+//   // console.log('Target param  is ', target);
+
+//   //switch statement?
+//   if(cards[start].value !== 13 &&
+//   board[targetArr].length === 0){
+//     console.log('NO MOVE: card is not a king!');
+//     return;
+//   //Move king into empty array
+//   }else if(cards[start].value === 13 && 
+//   board[targetArr].length === 0){
+//     //find targets array
+//     board[targetArr].push(board[startArr].pop());
+//     addCardImg(targetArr);
+//     removeCardImg();
+//     console.log('MOVE: king card added to empty column!');
+//     return;
+//   //move card on top of card with alt color
+//   }else if(cards[start].color !== cards[target].color &&
+//   //move card on top of card with +1 higher value 
+//   (cards[start]).value + 1 === (cards[target]).value){
+//     //find targets array
+//     board[targetArr].push(board[startArr].pop());
+//     addCardImg(targetArr);
+//     removeCardImg();
+//     console.log('card moved!');
+//     return;
+//   }else{
+//     console.log('card cannot move');
+//     return;
+//   }
+// }
+
+// //logic that determines if card can be moved to sideline storage area
+// function moveSuit(start, target, targetArr){
+//   //REPEAT - can turn into function
+//   for(var area in board){
+//     if(board[area].indexOf(start) >= 0){
+//       startArr = area;
+//     }
+//   }
+//   // console.log('start is ', start, 'target is : ', target, 'startArr is: ', startArr, 'targetArr is :', targetArr);
+//   //start card is not same suit
+//   if(cards[start].suit !== targetArr){
+//     console.log('suit does not match!');
+//     return;
+//   }else if(cards[start].value !== 1 && board[targetArr].length === 0){
+//     console.log('not an Ace, idiot!');
+//     return;
+//   }else if(cards[start].value === 1 && board[targetArr].length === 0){
+//     board[targetArr].push(board[startArr].pop());
+//     addCardImg(targetArr);
+//     removeCardImg();
+//     console.log('added Ace');
+//     return;
+//   }else if((cards[start]).value === (cards[target].value+1)){
+//     board[targetArr].push(board[startArr].pop());
+//     removeCardImg();
+//     console.log('stored suit');
+//     return;
+//   }else{
+//     console.log('card is not 1+ in value');
+//   }
+// }
 
 
 
@@ -265,7 +339,7 @@ $('.deck').click(cycle);
 //adds event listener to columns
 function addClick(){
   $('.flipped').off('click');
-  $('.flipped').click(decider);
+  $('.flipped').click(move);
 }
 
 //present info in console
