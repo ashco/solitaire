a985a4ae633495cd558357e669445f902d98e949
ashco <ashchristie@gmail.com>
2017-12-15 15:56:54 -0800

    did thing

--- js/source.js
+++ js/source.js
@@ -210,11 +210,11 @@ function moveCard(startNum, startArr, targetNum, targetArr){
   //move card on top of card with alt color && move card on top of card with +1 higher value 
   }else if(cards[startNum].color !== cards[targetNum].color && (cards[startNum]).value + 1 === (cards[targetNum]).value){
     
-    // console.log('targetArr before', targetArr);
+    console.log('board[targetArr] before', board[targetArr]);
     console.log(board[startArr].splice(-moveSize));
-    // targetArr = board[targetArr].concat(board[startArr].splice(-moveSize));
+    board[targetArr] = board[targetArr].concat(board[startArr].splice(-moveSize));
 
-    // console.log('targetArr after', targetArr);
+    console.log('board[targetArr] after', board[targetArr]);
 
 
     // board[targetArr].push(board[startArr].pop());
